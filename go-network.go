26689bc6345106d16ac66c78e345ddea503b6156
Kun <quinnHugh@hotmail.com>
2019-04-08 12:30:54 +0800

    update

--- go-network.go
+++ go-network.go
@@ -12,8 +12,8 @@ func main() {
 	v := url.Values{}
 	v.Set("action", "login")
 
-	v.Set("username", "xxxx")
-	v.Set("password", "xxxx")
+	v.Set("username", " ")
+	v.Set("password", " ")
 	v.Set("ac_id", "1")
 	v.Set("user_ip", "")
 	v.Set("nas_ip", "")
@@ -25,19 +25,20 @@ func main() {
 	client := &http.Client{}                                //客户端,被Get,Head以及Post使用
 	reqest, err := http.NewRequest("POST", "https://10.108.255.249/include/auth_action.php", body)
 	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
+		fmt.Println("Fatal error 1", err.Error())
 	}
 	//给一个key设定为响应的value.
 	reqest.Header.Set("Content-Type", "application/x-www-form-urlencoded") //必须设定该参数,POST参数才能正常提交
 
 	resp, err := client.Do(reqest) //发送请求
 	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
+		fmt.Println("Fatal error 2", err.Error())
+		fmt.Println(reqest)
 	}
 	defer resp.Body.Close() //一定要关闭resp.Body
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
+		fmt.Println("Fatal error 3", err.Error())
 	}
 
 	fmt.Println(string(content))
