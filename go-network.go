6d15fe5d12de7a058113e8a8847d4977f10ce6c0
llfl <i@imux.top>
2019-04-08 13:28:27 +0800

    update

--- go-network.go
+++ go-network.go
@@ -6,9 +6,13 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"crypto/tls"
 )
 
 func main() {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
 	v := url.Values{}
 	v.Set("action", "login")
 
@@ -21,8 +25,11 @@ func main() {
 	v.Set("save_me", "1")
 	v.Set("ajax", "1")
 	//利用指定的method,url以及可选的body返回一个新的请求.如果body参数实现了io.Closer接口，Request返回值的Body 字段会被设置为body，并会被Client类型的Do、Post和PostFOrm方法以及Transport.RoundTrip方法关闭。
-	body := ioutil.NopCloser(strings.NewReader(v.Encode())) //把form数据编下码
-	client := &http.Client{}                                //客户端,被Get,Head以及Post使用
+	//body := ioutil.NopCloser(strings.NewReader(v.Encode())) //把form数据编下码
+	body := strings.NewReader(v.Encode())
+	client := &http.Client{
+		Transport: tr,
+	}                                //客户端,被Get,Head以及Post使用
 	reqest, err := http.NewRequest("POST", "https://10.108.255.249/include/auth_action.php", body)
 	if err != nil {
 		fmt.Println("Fatal error 1", err.Error())
@@ -33,7 +40,6 @@ func main() {
 	resp, err := client.Do(reqest) //发送请求
 	if err != nil {
 		fmt.Println("Fatal error 2", err.Error())
-		fmt.Println(reqest)
 	}
 	defer resp.Body.Close() //一定要关闭resp.Body
 	content, err := ioutil.ReadAll(resp.Body)
